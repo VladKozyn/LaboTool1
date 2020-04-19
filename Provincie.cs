13c12cf7468d408e83fedf391e88acf3dbb1b466
vladik kozynin <liekesliefhebber2000@hotmail.com>
2020-04-19 14:18:36 +0200

    f

---
+++ Provincie.cs
@@ -0,0 +1,22 @@
+using System.Collections.Generic;
+
+namespace Tool1
+{
+    class Provincie
+    {
+        //  public Graaf graaf { get; set; }
+        // public int straatID { get; set; }
+        //  public string straatnaam { get; set; }
+
+
+        public void showGemeente()
+        {
+
+        }
+        public Provincie(//?????)
+        {
+
+        }
+
+    }
+}
