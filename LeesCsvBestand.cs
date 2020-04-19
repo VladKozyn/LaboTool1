26a7483047717e65660f7eecbd2a695ceb8d8e73
vladik kozynin <liekesliefhebber2000@hotmail.com>
2020-04-19 13:14:09 +0200

    f

--- LeesCsvBestand.cs
+++ LeesCsvBestand.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-
 namespace Tool1
 {
     class LeesCsvBestand
@@ -12,7 +11,6 @@ namespace Tool1
             List<List<String>> ListGesorteerdData = new List<List<string>>();
             using (StreamReader sr = new StreamReader("C:/Users/lieke/OneDrive/scool/prog 3/Labo/1/repository/WRdata.csv"))
             {
-               
                 string[] currentLineChopped;
                 int LineTeller = 0;
                 string currentLine;
@@ -24,7 +22,7 @@ namespace Tool1
                     {
                         ListGesorteerdData.Add(new List<String>());
                         currentLineChopped = currentLine.Split(';');
-                        for (int i = 0; i < 8; i++)
+                        for (int i = 0; i < currentLineChopped.Length; i++)
                         {
 
                             ListGesorteerdData[LineTeller].Add(currentLineChopped[i]);
@@ -49,7 +47,7 @@ namespace Tool1
         public List<List<String>> StraatNamenData()
         {
             List<List<String>> ListGesorteerdStraten = new List<List<string>>();
-            using (StreamReader sr = new StreamReader("C:/Users/lieke/OneDrive/scool/prog 3/Labo/1/repository/WRdata.csv"))
+            using (StreamReader sr = new StreamReader("C:/Users/lieke/OneDrive/scool/prog 3/Labo/1/repository/WRstraatnamen.csv"))
             {
                
                 string[] currentLineChopped;
@@ -59,19 +57,125 @@ namespace Tool1
                 // currentLine moet null zijn als het einde bereikt
                 while ((currentLine = sr.ReadLine()) != null)
                 {
-                        ListGesorteerdData.Add(new List<String>());
+                    if (!currentLine.Contains("-9"))
+                    {
+                        ListGesorteerdStraten.Add(new List<String>());
                         currentLineChopped = currentLine.Split(';');
-                        for (int i = 0; i < 8; i++)
+                        for (int i = 0; i < currentLineChopped.Length; i++)
                         {
 
-                            ListGesorteerdData[LineTeller].Add(currentLineChopped[i]);
+                            ListGesorteerdStraten[LineTeller].Add(currentLineChopped[i]);
 
                         }
                         LineTeller++;
-
+                        }
                 }
             }
-            return ListGesorteerdData;
+            return ListGesorteerdStraten;
         }
+        //[0] ID
+        //[1] Naam
+        public List<List<String>> GemeentenaamData()
+        {
+            List<List<String>> ListGesorteerdGemeenteNamen = new List<List<string>>();
+            using (StreamReader sr = new StreamReader("C:/Users/lieke/OneDrive/scool/prog 3/Labo/1/repository/WRGemeentenaam.csv"))
+            {
+               
+                string[] currentLineChopped;
+                int LineTeller = 0;
+                string currentLine;
+                sr.ReadLine();
+                // currentLine moet null zijn als het einde bereikt
+                while ((currentLine = sr.ReadLine()) != null)
+                {
+
+                        ListGesorteerdGemeenteNamen.Add(new List<String>());
+                        currentLineChopped = currentLine.Split(';');
+                        for (int i = 0; i < currentLineChopped.Length; i++)
+                        {
+
+                            ListGesorteerdGemeenteNamen[LineTeller].Add(currentLineChopped[i]);
+
+                        }
+                        LineTeller++;
+                        
+                }
+            }
+            return ListGesorteerdGemeenteNamen;
+        }
+        /*
+         [0] gemeentenaamID
+         [1] gemeenteID
+         [2] taalCodeGemeenteNaam
+         [3] gemeenteNaam
+        */
+        public List<List<String>> GemeenteIdData()
+        {
+            List<List<String>> ListGesorteerdGemeenteId = new List<List<string>>();
+            using (StreamReader sr = new StreamReader("C:/Users/lieke/OneDrive/scool/prog 3/Labo/1/repository/WRGemeenteID.csv"))
+            {
+               
+                string[] currentLineChopped;
+                int LineTeller = 0;
+                string currentLine;
+                sr.ReadLine();
+                // currentLine moet null zijn als het einde bereikt
+                while ((currentLine = sr.ReadLine()) != null)
+                {
+
+                        ListGesorteerdGemeenteId.Add(new List<String>());
+                        currentLineChopped = currentLine.Split(';');
+                        for (int i = 0; i < currentLineChopped.Length; i++)
+                        {
+
+                            ListGesorteerdGemeenteId[LineTeller].Add(currentLineChopped[i]);
+
+                        }
+                        LineTeller++;
+                        
+                }
+            }
+            return ListGesorteerdGemeenteId;
+        }
+        // [0] straatNaamId
+        // [1] gemeenteID
+
+        public List<List<String>> ProvincieInfoData()
+        {
+            List<List<String>> ListGesorteerdProvincieInfo = new List<List<string>>();
+              StreamReader provincieIdLeesCheck = new StreamReader("C:/Users/lieke/OneDrive/scool/prog 3/Labo/1/repository/ProvincieIDsVlaanderen.csv");
+               String provincieIdCheckMetComma = provincieIdLeesCheck.ReadLine();
+               String[] provincieIdCheck = provincieIdCheckMetComma.Split(',');
+            using (StreamReader sr = new StreamReader("C:/Users/lieke/OneDrive/scool/prog 3/Labo/1/repository/ProvincieInfo.csv"))
+            {
+               
+                string[] currentLineChopped;
+                int LineTeller = 0;
+                string currentLine;
+                 sr.ReadLine();
+
+                // currentLine moet null zijn als het einde bereikt
+                while ((currentLine = sr.ReadLine()) != null)
+                {
+                  for (int f = 0; f < provincieIdCheck.Length; f++) {
+                    if (currentLine.Contains(";"+f+";"))
+                    {
+                        ListGesorteerdProvincieInfo.Add(new List<String>());
+                        currentLineChopped = currentLine.Split(';');
+                        for (int i = 0; i < currentLineChopped.Length; i++)
+                        {
+
+                            ListGesorteerdProvincieInfo[LineTeller].Add(currentLineChopped[i]);
+
+                        }
+                        LineTeller++;
+                        }
+                    }
+                }
+            }
+            return ListGesorteerdProvincieInfo;
+        }
+
+
     }
 }
