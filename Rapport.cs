2645de7c6042ffdaeb5777d4c886762218f72a9a
vladik kozynin <liekesliefhebber2000@hotmail.com>
2020-04-20 09:29:33 +0200

    f

--- Rapport.cs
+++ Rapport.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 
 namespace Tool1
 {
@@ -12,13 +13,26 @@ namespace Tool1
             List<Segment> listMetWegsegmenten = null;
             for (int i = 0; i < wegSegmentenListText.Count; i++)
             {
-                List<Punt> f = new List<Punt>();
-                ListGesorteerdData[LineTeller].Add(f);
-                int wegsegmentID = int.Parse(wegSegmentenListText[i][0]);
-                Knoop BeginKnoop = new Knoop(int.Parse(wegSegmentenListText[i][4]), new Punt(wegSegmentenListText[i][1]),new Punt);
-                Knoop eindKnoop = new Knoop(int.Parse(wegSegmentenListText[i][4]), new Punt
+                List<Double> doubleListVanPunten = new List<Double>();
+                List<Punt> listVanPuntenMetDoubleValue = new List<Punt>();
+                string puntenPlainText = wegSegmentenListText[i][1];
+                puntenPlainText.Remove(0, 12);
+                puntenPlainText.Trim(',');
+               String[] puntenArrayStrings = puntenPlainText.Split(" ");
+                for (int k = 0; k < puntenArrayStrings.Length; k++)
+                {
+                    doubleListVanPunten.Add(double.Parse(puntenArrayStrings[k], CultureInfo.InvariantCulture));
+                    if(!(k % 2 == 0))
+                    {
+                        listVanPuntenMetDoubleValue.Add(new Punt(doubleListVanPunten[k - 1], doubleListVanPunten[k]));
+                    }
+                }
 
-                listMetWegsegmenten.Add(new Segment(wegsegmentID, ), wegSegmentenListText[5], wegSegmentenListText[1])); ;
+                int wegsegmentID = int.Parse(wegSegmentenListText[i][0]);
+                Knoop beginKnoop = new Knoop(int.Parse(wegSegmentenListText[i][4]),listVanPuntenMetDoubleValue[0]);
+                Knoop eindKnoop = new Knoop(int.Parse(wegSegmentenListText[i][5]), listVanPuntenMetDoubleValue[listVanPuntenMetDoubleValue.Count-1]);
+
+                listMetWegsegmenten.Add(new Segment(wegsegmentID, beginKnoop, eindKnoop, listVanPuntenMetDoubleValue)); ;
 
                         //                if(int.Parse(wegsegmentenList[i][4]) == i|| int.Parse(wegsegmentenList[i][5]) == i)
                         //als wegsegment gebruikt voor links en rechts voor dezelfde graaf het verwijderen
